6eecee6415d8dcbe183a2ae498d2a3c82add0134
kitu <kristoftunner95@gmail.com>
2022-10-12 22:18:41 +0200

    added plot class

---
+++ src/core.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace gui
+{
+  template<typename T>
+  using Ref = std::shared_ptr<T>;
+}// namespace
---
+++ src/plot2d.cpp
@@ -0,0 +1,67 @@
+#include "plot2d.h"
+
+#include <qt5/QList>
+
+namespace gui
+{
+
+PlotWidget::PlotWidget(QWidget *parent, const std::string& xLabel, const std::string& yLabel, const std::string& title) :
+  QWidget(parent),
+  m_chart(new QtCharts::QChart),
+  m_series(new QtCharts::QLineSeries),
+  m_lowerBound(0), m_upperBound(10), m_leftBound(0), m_rightBound(10)
+{
+  QChartView *chartView = new QChartView(m_chart);
+  m_chart->addSeries(m_series);
+  m_axisX = new QValueAxis;
+  m_axisX->setRange(m_leftBound, m_rightBound);
+  m_axisX->setLabelFormat("%g");
+  m_axisX->setTitleText(xLabel.c_str());
+  m_axisY = new QValueAxis;
+  m_axisY->setRange(m_lowerBound, m_upperBound);
+  m_axisY->setTitleText(yLabel.c_str());
+  m_chart->addAxis(m_axisX, Qt::AlignBottom);
+  m_series->attachAxis(m_axisX);
+  m_chart->addAxis(m_axisY, Qt::AlignLeft);
+  m_series->attachAxis(m_axisY);
+  m_chart->legend()->hide();
+  m_chart->setTitle(title.c_str());
+
+  QVBoxLayout *mainLayout = new QVBoxLayout(this);
+  mainLayout->addWidget(chartView);
+}
+
+void PlotWidget::SetXAxisRange(int leftBound, int rigthBound)
+{
+  m_leftBound = leftBound;
+  m_rightBound = rigthBound;
+  m_axisX->setRange(m_leftBound, m_rightBound);
+}
+
+void PlotWidget::SetYAxisRange(int lowerBound, int upperBound)
+{
+  m_lowerBound = lowerBound;
+  m_upperBound = upperBound;
+  m_axisY->setRange(m_lowerBound, m_upperBound);
+}
+
+void PlotWidget::UpdateXData(const PlotData& plotData)
+{
+  // generate QList<QPointF>
+  QList<QPointF> data;
+  for(const auto& dataPoint : plotData)
+  {
+    data.push_back(dataPoint.first, dataPoint.second);
+  }
+
+  // update chart data
+  m_series->replace(data);
+}
+
+const PlotData& PlotWidget::GetXData() const
+{
+  return m_series->points();
+}
+
+
+} // namespace gui
